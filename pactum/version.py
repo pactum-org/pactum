83eca6a815ba968d511915c59fa88f60a351f274
Eduardo Stalinho <eduardo.carvalho@olist.com>
2018-11-12 17:04:07 -0200

    Allow class definition for version.

--- pactum/version.py
+++ pactum/version.py
@@ -4,13 +4,21 @@ from .route import Route
 
 class Version:
     def __init__(self, name=None, routes=None, resources=None):
-        if routes is not None and resources is not None:
-            raise TypeError("Cannot define version with both routes and resources.")
-        if routes is None and resources is None:
-            raise TypeError("Version must have routes or resources.")
+        if name is None:
+            name = getattr(self, 'name', '')
         self.name = name
+        if routes is None:
+            routes = getattr(self, 'routes', None)
         self.routes = routes
+        if resources is None:
+            resources = getattr(self, 'resources', None)
+
         self.resources = resources
+
+        if self.routes is not None and self.resources is not None:
+            raise TypeError("Cannot define version with both routes and resources.")
+        if self.routes is None and self.resources is None:
+            raise TypeError("Version must have routes or resources.")
 
     def append(self, thing):
         if isinstance(thing, Route) and self.resources is None:
--- tests/test_version.py
+++ tests/test_version.py
@@ -14,6 +14,17 @@ def test_basic_version_with_routes():
     assert len(version.routes) == 0
 
 
+def test_version_class_definition_with_routes():
+    class V1(Version):
+        name = "V1"
+        routes = []
+
+    version = V1()
+
+    assert version.name == "V1"
+    assert len(version.routes) == 0
+
+
 def test_basic_version_with_resources():
     version = Version(
         name="1.0",
@@ -24,14 +35,38 @@ def test_basic_version_with_resources():
     assert len(version.resources) == 0
 
 
-def test_fail_version_with_routes_and_resources():
+def test_version_class_definition_with_resources():
+    class V1(Version):
+        name = "V1"
+        resources = []
+
+    version = V1()
+
+    assert version.name == "V1"
+    assert len(version.resources) == 0
+
+
+def test_fail_version_class_with_routes_and_resources():
+    class V1(Version):
+        name = "V1"
+        resources = []
+        routes = []
+
     with pytest.raises(TypeError):
-        Version(name="v0", routes=[], resources=[])
+        V1()
 
 
 def test_fail_if_no_routes_and_no_version():
     with pytest.raises(TypeError):
         Version(name="v1")
+
+
+def test_class_def_fails_if_no_routes_and_no_version():
+    class V1(Version):
+        name = "V1"
+
+    with pytest.raises(TypeError):
+        V1()
 
 
 def test_append_route(route):
