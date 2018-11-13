acfde1bc8ba608c2cfad897ad5035b71d5c4a509
Osvaldo Santana Neto <osantana@osantana.me>
2018-11-13 15:28:24 -0200

    Add resource fields relation constraints.

--- pactum/resources.py
+++ pactum/resources.py
@@ -1,13 +1,25 @@
+
+
 class Resource:
     def __init__(self, name=None, fields=None, behaviors=None):
+        self._mapfields = {}
+
         if name is None:
             name = getattr(self, "name", self.__class__.__name__)
         self.name = name
 
         if fields is None:
             fields = getattr(self, "fields", [])
+
+        for field in fields:
+            if self._mapfields.get(field.name):
+                raise AttributeError("Duplicate field names")
+            self._mapfields[field.name] = field
         self.fields = fields
 
         if behaviors is None:
             behaviors = getattr(self, "behavior", [])
         self.behaviors = behaviors
+
+    def __getitem__(self, item):
+        return self._mapfields[item]
--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,3 +1,6 @@
+import pytest
+
+from pactum.fields import Field
 from pactum.resources import Resource
 
 
@@ -34,3 +37,48 @@ def test_basic_resource_class_definition_with_class_name():
     assert resource.name == "MyResource"
     assert len(resource.fields) == 0
     assert len(resource.behaviors) == 0
+
+
+def test_access_fields_by_key():
+    class MyResource(Resource):
+        fields = [
+            Field(name='my_field'),
+            Field(name='my_other_field')
+        ]
+
+    resource = MyResource()
+
+    assert isinstance(resource['my_field'], Field)
+    assert isinstance(resource['my_other_field'], Field)
+
+    assert resource['my_field'].name == 'my_field'
+    assert resource['my_other_field'].name == 'my_other_field'
+
+def test_duplicate_field_names_raises_error():
+    class MyResource(Resource):
+        fields = [
+            Field(name='my_field'),
+            Field(name='my_field')
+        ]
+
+    with pytest.raises(AttributeError):
+        MyResource()
+
+def test_subclassing_add_fields():
+    class MyResource(Resource):
+        fields = [
+            Field(name='my_field'),
+        ]
+
+    class MySubResource(MyResource):
+        fields = MyResource.fields + [
+            Field(name='my_other_field')
+        ]
+
+    resource = MySubResource()
+
+    assert isinstance(resource['my_field'], Field)
+    assert resource['my_field'].name == 'my_field'
+
+    assert isinstance(resource['my_other_field'], Field)
+    assert resource['my_other_field'].name == 'my_other_field'
