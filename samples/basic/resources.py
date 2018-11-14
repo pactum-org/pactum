93b4ade96bcb54f98b3d193fb427562356d2ad3a
Osvaldo Santana Neto <osantana@osantana.me>
2018-11-14 17:09:25 -0200

    Remove deprecated sample

--- samples/basic/resources.py
+++
@@ -1,3 +0,0 @@
-from resources import ResourceList
-
-products = ResourceList()
--- samples/basic/versions.py
+++
@@ -1,7 +0,0 @@
-from pactum.version import Version, version_selector
-
-
-class FirstVersion(Version):
-    class Meta:
-        name = "1.0"
-        selector = version_selector("/v1")
