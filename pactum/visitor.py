1490962bbc243a28076c82c0e6f60edf0cc6c1c5
Eduardo Stalinho <eduardo.carvalho@olist.com>
2018-11-22 15:51:07 -0200

    Raises NotImplementedError instead of dummy printing in BaseVisitor.

--- pactum/visitor.py
+++ pactum/visitor.py
@@ -1,27 +1,27 @@
 class BaseVisitor:
     def visit_api(self, api):
-        print(f'Visiting API: {api.name}')
+        raise NotImplementedError('visit_api is not implemented')
 
     def visit_version(self, version):
-        print(f'Visiting Version {version.name} for {version.parent.name}')
+        raise NotImplementedError('visit_version is not implemented')
 
     def visit_route(self, route):
-        print(f'Visiting Route {route.path}')
-
-    def visit_request(self, request):
-        print(f'Visiting Request {request.verb}')
-
-    def visit_response(self, response):
-        print(f'Visiting Response {response.status}, {[x for x in response.headers]}')
+        raise NotImplementedError('visit_route is not implemented')
 
     def visit_action(self, action):
-        print(f'Visition action')
+        raise NotImplementedError('visit_action is not implemented')
+
+    def visit_request(self, request):
+        raise NotImplementedError('visit_request is not implemented')
+
+    def visit_response(self, response):
+        raise NotImplementedError('visit_response is not implemented')
 
     def visit_resource(self, resource):
-        print(f'Visiting Resource {resource.name}.')
+        raise NotImplementedError('visit_resource is not implemented')
 
     def visit_field(self, field):
-        print(f'Visiting Field {field.name}, {field.type}')
+        raise NotImplementedError('visit_field is not implemented')
 
     def visit_list_resource(self, list_resource):
-        pass
+        raise NotImplementedError('visit_list_resource is not implemented')
