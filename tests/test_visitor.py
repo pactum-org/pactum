42d2a3ed956e44d39ed83bbb45d87cd74004b0e5
Eduardo Stalinho <eduardo.carvalho@olist.com>
2018-12-31 12:26:06 -0200

    Test visit accepts element by element.

--- tests/test_visitor.py
+++ tests/test_visitor.py
@@ -1,7 +1,11 @@
+import pytest
+
+from pactum import fields, verbs
+from pactum import API, Version, Route, Action, Request, Response, Querystring, Resource, ListResource
 from pactum.visitor import BaseVisitor
 
 
-class TestVisitor(BaseVisitor):
+class BlueprintVisitor(BaseVisitor):
     def __init__(self):
         self.blueprints = []
 
@@ -33,24 +37,118 @@ class TestVisitor(BaseVisitor):
         self.blueprints.append('field visited')
 
     def visit_list_resource(self, list_resource):
-        self.blueprints.append('list_resource visited')
+        self.blueprints.append('list resource visited')
 
 
-def test_visitor_call_order(api):
-    visitor = TestVisitor()
+@pytest.fixture
+def visitor():
+    return BlueprintVisitor()
+
+
+def test_visitor_api_call(visitor):
+    api = API(versions=[Version(name='v1', routes=[])])
     api.accept(visitor)
 
     assert visitor.blueprints == [
         'api visited',
         'version visited',
+    ]
+
+
+def test_visitor_version_call(visitor):
+    version = Version(name='v1', routes=[Route(path='/test')])
+    version.accept(visitor)
+
+    assert visitor.blueprints == [
+        'version visited',
+        'route visited',
+    ]
+
+
+def test_visitor_route_call(visitor):
+    route = Route(path='/test', actions=[Action(responses=[])])
+    route.accept(visitor)
+
+    assert visitor.blueprints == [
         'route visited',
         'action visited',
+    ]
+
+
+def test_visitor_route_call_with_querystring(visitor):
+    route = Route(
+        path='/test', actions=[Action(responses=[])], querystrings=[Querystring(name='test_qs')])
+    route.accept(visitor)
+
+    assert visitor.blueprints == [
+        'route visited',
+        'action visited',
+        'querystring visited',
+    ]
+
+
+def test_visitor_action_call(visitor):
+    action = Action(request=Request(verb=verbs.GET), responses=[Response(status=200)])
+    action.accept(visitor)
+
+    assert visitor.blueprints == [
+        'action visited',
+        'request visisted',
+        'response visited',
+    ]
+
+
+def test_visitor_request_call(visitor):
+    request = Request(verb=verbs.GET, payload=Resource())
+    request.accept(visitor)
+
+    assert visitor.blueprints == [
         'request visisted',
         'resource visited',
-        'field visited',
-        'response visited',
-        'list_resource visited',
-        'resource visited',
-        'field visited',
-        'querystring visited'
     ]
+
+
+def test_visitor_response_call(visitor):
+    response = Response(status=200, body=Resource())
+    response.accept(visitor)
+
+    assert visitor.blueprints == [
+        'response visited',
+        'resource visited',
+    ]
+
+
+def test_visitor_resource_call(visitor):
+    resource = Resource(fields=[fields.Field(name='test')])
+    resource.accept(visitor)
+
+    assert visitor.blueprints == [
+        'resource visited',
+        'field visited'
+    ]
+
+
+def test_visitor_list_resource_call(visitor):
+    list_resource = ListResource(resource=Resource())
+    list_resource.accept(visitor)
+
+    assert visitor.blueprints == [
+        'list resource visited',
+        'resource visited'
+    ]
+
+
+def test_visitor_field_call(visitor):
+    field = fields.Field(name='test')
+    field.accept(visitor)
+
+    assert visitor.blueprints == [
+        'field visited'
+    ]
+
+
+def test_visitor_querystring_call(visitor):
+    querystring = Querystring()
+    querystring.accept(visitor)
+
+    assert visitor.blueprints == ['querystring visited']
